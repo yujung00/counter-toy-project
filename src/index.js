5d30ce92398c97f74dbd8186e558da3d67dcc479
yujung00 <ung5468@naver.com>
2022-06-30 15:29:22 +0900

    🧑‍💻 chorm 개발자 도구 reducre 사용

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reducers from './reducres';
 import {Provider} from "react-redux";
 
 // store 생성
-const store = createStore(reducers);
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
